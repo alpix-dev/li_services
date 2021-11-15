8cd48c7e7cc23530b040e9f9b9ee32bb071c6e7d
eduvlemes <edu.vlemes@gmail.com>
2021-11-15 12:36:53 -0300

    cupom e inserção de banners

--- autoDiscount/autoDiscount.1.0.js
+++ autoDiscount/autoDiscount.1.0.js
@@ -1,29 +1,53 @@
 apx_widgets.worker.autoDiscount = {};
 apx_widgets.worker.autoDiscount.list = [];
-//apx_widgets.worker.autoDiscount.list.push({coupom:"ALPIX5",min_subtotal:1000});
 apx_widgets.worker.autoDiscount.functions = {};
-apx_widgets.worker.autoDiscount.style = "<style>/* AUTO COUPOM */label[for='usarCupom']{display:none}.remover-cupom{display:none;}</style>";
 apx_widgets.worker.autoDiscount.config = {};
 apx_widgets.worker.autoDiscount.match = $('.pagina-carrinho').length > 0;
 apx_widgets.worker.autoDiscount.run = function(){
-    $('label[for="usarCupom"]').closest('tr').hide();
-    let subtotal = parseFloat($('[data-subtotal-valor]').attr('data-subtotal-valor'));
     
-    let query_ = apx_widgets.worker.autoDiscount.list.filter(el => parseFloat(el.min_subtotal) <= subtotal);
-    console.log(query_);
+    let subtotal = $('.carrinho-checkout').length > 0 ? parseFloat($('[data-subtotal]').attr('data-subtotal').replace('.','').replace(',','.')) : parseFloat($('[data-subtotal-valor]').attr('data-subtotal-valor'));    
+    
+    let activeCoupom = $('.carrinho-checkout').length > 0 ? $('#inputCupom').val() : $('.possui-cupom a + input').val();
+    
+    if(activeCoupom != null){
+        let query_ = apx_widgets.worker.autoDiscount.list.filter(el => el.coupom == activeCoupom);    
+        if(parseInt(query_[0].min_subtotal) > parseInt(subtotal)){
+            let url = "/carrinho/cupom/remover?cupom=" + activeCoupom;
+            $.get( url, function( data ) {
+                location.reload(); 
+            });
+        }
+    }
+    
+    let query_ = apx_widgets.worker.autoDiscount.list.filter(el => parseFloat(el.min_subtotal) <= subtotal);    
     if(query_.length > 0){
         var higher;
         for (var i=0 ; i<query_.length ; i++) {
             if (higher == null || parseInt(query_[i].min_subtotal) > parseInt(higher.min_subtotal))
             higher = query_[i];
         }
-        console.log(higher);
-
-        $('#usarCupom').val(higher.coupom);
-        $('#usarCupom').closest('form').submit();
+    
+        //console.log(localStorage.getItem(higher.coupom));
+        if($('.carrinho-checkout').length > 0){
+            if($('.carrinho-checkout #inputCupom').length == 0 && localStorage.getItem(higher.coupom) != 'true'){            
+                $.post("/carrinho/cupom/validar", {cupom: higher.coupom}, function(result){                    
+                    location.reload(); 
+                });
+            }
+        }else{
+            if($('#usarCupom').length > 0 && localStorage.getItem(higher.coupom) != 'true'){            
+                $.post("/carrinho/cupom/validar", {cupom: higher.coupom}, function(result){
+                    location.reload(); 
+                });
+            }
+        }
     }
 
-    console.log('apx_widgets.worker.autoDiscount OK');     
     
+
+    $('.possui-cupom a').click(function(){
+        localStorage.setItem($(this).next('input').val(),true);
+    })
+    console.log('apx_widgets.worker.autoDiscount OK');         
 }
 
---
+++ insertBanners/insertBanners.1.0.js
@@ -0,0 +1,36 @@
+apx_widgets.worker.insertBanners = {};
+apx_widgets.worker.insertBanners.list = [];
+//apx_widgets.worker.insertBanners.list.push({target:'Posição',position:2,title:'',images:[{url:'https://cdn.awsli.com.br/1984/1984900/arquivos/585x200_a.png', link:'#'},{url:'https://cdn.awsli.com.br/1984/1984900/arquivos/585x200_a.png', link:''},{url:'https://cdn.awsli.com.br/1984/1984900/arquivos/585x200_a.png', link:''},{url:'https://cdn.awsli.com.br/1984/1984900/arquivos/585x200_a.png', link:''}]});
+apx_widgets.worker.insertBanners.functions = {};
+apx_widgets.worker.insertBanners.style = "";
+apx_widgets.worker.insertBanners.config = {text:"Ver Todos"};
+apx_widgets.worker.insertBanners.match = 1 == 1;
+apx_widgets.worker.insertBanners.run = function(){
+    $.each(apx_widgets.worker.insertBanners.list, function(k, item){
+        let targetSelector = null;
+        switch (item.target){
+            case 'Lançamento':
+                targetSelector = $('.vitrine-lancamento + ul');
+                break;
+            case 'Mais Vendidos':
+                targetSelector = $('.vitrine-mais-vendidos + ul');
+            break;
+            case 'Posição':
+                targetSelector = $('.pagina-inicial #listagemProdutos > ul').eq(item.position - 1);
+            break; 
+        }
+         
+        if(targetSelector.length == 1 && item.images.length > 0){
+            console.log('entrou');
+            let columns = 12/item.images.length;
+            let row = $('<div class="row-fluid"></div>');
+            $.each(item.images,function(k2, image){
+                row.append('<div class="span'+ columns+'"><a href="'+ image.link +'"><img src="'+ image.url +'"/></a></div>');
+            });
+            targetSelector.after(row);
+        }else{
+            console.log('nao entrou')
+        }
+    })  
+}
+
