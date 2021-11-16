2180ab907931189945e82c4333ea4305a7bd95c3
eduvlemes <edu.vlemes@gmail.com>
2021-11-16 00:18:23 -0300

    remove log banners

--- insertBanners/insertBanners.1.0.js
+++ insertBanners/insertBanners.1.0.js
@@ -3,7 +3,7 @@ apx_widgets.worker.insertBanners.list = [];
 //apx_widgets.worker.insertBanners.list.push({target:'Posição',position:2,title:'',images:[{url:'https://cdn.awsli.com.br/1984/1984900/arquivos/585x200_a.png', link:'#'},{url:'https://cdn.awsli.com.br/1984/1984900/arquivos/585x200_a.png', link:''},{url:'https://cdn.awsli.com.br/1984/1984900/arquivos/585x200_a.png', link:''},{url:'https://cdn.awsli.com.br/1984/1984900/arquivos/585x200_a.png', link:''}]});
 apx_widgets.worker.insertBanners.functions = {};
 apx_widgets.worker.insertBanners.style = "";
-apx_widgets.worker.insertBanners.config = {text:"Ver Todos"};
+apx_widgets.worker.insertBanners.config = {};
 apx_widgets.worker.insertBanners.match = 1 == 1;
 apx_widgets.worker.insertBanners.run = function(){
     $.each(apx_widgets.worker.insertBanners.list, function(k, item){
@@ -21,15 +21,12 @@ apx_widgets.worker.insertBanners.run = function(){
         }
          
         if(targetSelector.length == 1 && item.images.length > 0){
-            console.log('entrou');
             let columns = 12/item.images.length;
             let row = $('<div class="row-fluid"></div>');
             $.each(item.images,function(k2, image){
                 row.append('<div class="span'+ columns+'"><a href="'+ image.link +'"><img src="'+ image.url +'"/></a></div>');
             });
             targetSelector.after(row);
-        }else{
-            console.log('nao entrou')
         }
     })  
 }
