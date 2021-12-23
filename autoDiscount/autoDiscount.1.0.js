af312f3235790c6b68037cdfcba88f7d07c8873a
eduvlemes <edu.vlemes@gmail.com>
2021-12-22 21:38:36 -0300

    autoDiscount 2

--- autoDiscount/autoDiscount.1.0.js
+++ autoDiscount/autoDiscount.1.0.js
@@ -40,31 +40,44 @@ apx_widgets.worker.autoDiscount.run = function(){
                 availableCoupons.push({cupom:item.coupom, description:item.description});
             }        
         });
-    }
 
-    if(availableCoupons.length > 0){
-        if(availableCoupons.length == 1){
-            console.log(availableCoupons)
-            if($('.carrinho-checkout').length > 0){
-                if($('.carrinho-checkout #inputCupom').length == 0 && sessionStorage.getItem(availableCoupons[0].cupom) != 'true'){            
-                    $.post("/carrinho/cupom/validar", {cupom: availableCoupons[0].cupom}, function(result){                    
-                        location.reload(); 
-                    });
+        if(availableCoupons.length > 0){
+            if(availableCoupons.length == 1){
+                if($('.carrinho-checkout').length > 0){
+                    if($('.carrinho-checkout #inputCupom').length == 0 && sessionStorage.getItem(availableCoupons[0].cupom) != 'true'){            
+                        $.post("/carrinho/cupom/validar", {cupom: availableCoupons[0].cupom}, function(result){                    
+                            location.reload(); 
+                        });
+                    }
+                }else{
+                    if($('#usarCupom').length > 0 && sessionStorage.getItem(availableCoupons[0].cupom) != 'true'){            
+                        $.post("/carrinho/cupom/validar", {cupom: availableCoupons[0].cupom}, function(result){
+                            location.reload(); 
+                        });
+                    }
                 }
             }else{
-                if($('#usarCupom').length > 0 && sessionStorage.getItem(availableCoupons[0].cupom) != 'true'){            
-                    $.post("/carrinho/cupom/validar", {cupom: availableCoupons[0].cupom}, function(result){
-                        location.reload(); 
-                    });
-                }
+                $('<tr id="apx_coupom"><td colspan="6"><strong class="cor-secundaria">'+ apx_widgets.worker.autoDiscount.config.title +'</strong><p>'+ apx_widgets.worker.autoDiscount.config.description +'</p><div></div></td></tr>').insertAfter($('#usarCupom').closest('tr'));        
+                $.each(availableCoupons,function(k,item){
+                    $('#apx_coupom > td > div').append('<div><form action="/carrinho/cupom/validar" method="post"><input name="cupom" type="hidden" value="'+ item.cupom +'"/><button type="submit">'+ item.cupom +'</button></form><p>'+ item.description +'</p></div>')
+                })
             }
-        }else{
-            $('<tr id="apx_coupom"><td colspan="6"><strong class="cor-secundaria">'+ apx_widgets.worker.autoDiscount.config.title +'</strong><p>'+ apx_widgets.worker.autoDiscount.config.description +'</p><div></div></td></tr>').insertAfter($('#usarCupom').closest('tr'));        
-            $.each(availableCoupons,function(k,item){
-                $('#apx_coupom > td > div').append('<div><form action="/carrinho/cupom/validar" method="post"><input name="cupom" type="hidden" value="'+ item.cupom +'"/><button type="submit">'+ item.cupom +'</button></form><p>'+ item.description +'</p></div>')
-            })
+        }
+        //console.log(activeCoupom)
+        if(activeCoupom !== undefined){
+            let existInList = apx_widgets.worker.autoDiscount.list.filter(el => el.coupom == activeCoupom);    
+            let isAvailable = availableCoupons.filter(el => el.cupom == activeCoupom);    
+            if(existInList.length > 0 && isAvailable.length == 0){
+                $.get('/carrinho/cupom/remover?cupom='+activeCoupom, function(result){                    
+                    location.reload(); 
+                });
+            }            
         }
     }
+    
+    
+
+    
     $('.possui-cupom a').click(function(){
         sessionStorage.setItem($(this).next('input').val(),true);
     })
